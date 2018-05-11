5c05d4dff973a2886055021e60c0cb310da35586
LunaGram <lunad3@uci.edu>
2018-05-11 14:29:10 -0700

    files created

---
+++ javascript/gameLogic.js
@@ -0,0 +1,35 @@
+//--------------------Characters and stats---------------------------
+var ObiWan = 
+    {
+        "HP":0,
+        "AP":0,
+        "CAP":0
+    };
+
+var Luke = 
+    {
+        "HP":0,
+        "AP":0,
+        "CAP":0
+    };
+
+var DarthMaul = 
+    {
+        "HP":0,
+        "AP":0,
+        "CAP":0
+    };
+
+var Vader = 
+    {
+        "HP":0,
+        "AP":0,
+        "CAP":0
+    };
+//-------------------------
+
+var attackBonus = 0;
+var playerSelected = false;
+var enemySelected = false;
+
+
