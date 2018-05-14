fac362cfb540d9ed2512e2109c0205153f9f78c6
LunaGram <lunad3@uci.edu>
2018-05-14 15:06:37 -0700

    new master, dynamic character gen

--- javascript/gameLogic.js
+++ javascript/gameLogic.js
@@ -1,35 +1,29 @@
 //--------------------Characters and stats---------------------------
-var ObiWan = 
-    {
-        "HP":0,
-        "AP":0,
-        "CAP":0
-    };
+var characterList = [];
 
-var Luke = 
+$("div.character").each(function()
     {
-        "HP":0,
-        "AP":0,
-        "CAP":0
-    };
-
-var DarthMaul = 
-    {
-        "HP":0,
-        "AP":0,
-        "CAP":0
-    };
-
-var Vader = 
-    {
-        "HP":0,
-        "AP":0,
-        "CAP":0
-    };
+        var char = 
+            {
+                "name": $(this).attr("id"),
+                "AP"  : parseInt($(this).attr("data-AP")),
+                "CAP" : parseInt($(this).attr("data-CAP")),
+                "HP"  : parseInt($(this).attr("data-HP"))
+            }
+        characterList.push(char);
+    });
 //-------------------------
-
 var attackBonus = 0;
+var player;
 var playerSelected = false;
 var enemySelected = false;
+
+$(".character").on("click",function()
+    {
+        if (!playerSelected)
+            {
+                player = 
+            }
+    });
 
 
